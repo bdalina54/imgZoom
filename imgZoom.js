eb70909c944de40fbe3ce0dbd492e1e2ba0b8510
Bradley B. Dalina <bdalina54@gmail.com>
2017-11-01 07:18:44 +0800

    Update imgZoom.js

--- imgZoom.js
+++ imgZoom.js
@@ -2,39 +2,46 @@
  * Image Zoom Viewer
  * By: Bradley B. Dalina
  * Started October 31, 2017
+ * Version 1.1 
+ * Updated November 1, 2017
 */
 
 (function(w, d)
 {
+    'use_strict';
     function _()
     {
         var $ = {};
-        $.zoom = function(el, ur, ti, de, da)//el=element, ur=image url, ti=title, de= description, da=date
+        $.zoom = function(el, ur)//el=element, ur=image url, ti=title, de= description, da=date
         {
             var pi = (typeof el ==='undefined') ? '.imgzoom' : el;  
             var img=d.querySelector(pi);  
             if(!img) {console.error('Error: Did not found any matching element of "' + pi+'" it returns '+img); return false;}
             var zL = d.getElementById('zoomLayer');
             if(!zL){$.objectLayer();}
-            img.onload = function()
-            {
-                
-                img.addEventListener("click", function (e) 
-                {
-                    var zImage = d.getElementById('zoomImage');
-                    var zLayer = d.getElementById('zoomLayer');
-                    var imgurl = (typeof ur === 'undefined') ? img.src : ur; 
-                    
-                    zImage.src=imgurl;
-                    zLayer.style.display ='block';
 
-                    zImage.onload = function() 
+                img.addEventListener("click", function (e) 
                     {
-                        $.resize(zImage.naturalWidth, zImage.naturalHeight);
-                    };
-                });
-                $.addEvent();
-            };
+                        var zImage = d.getElementById('zoomImage');
+                        var zLayer = d.getElementById('zoomLayer');
+                        var imgurl = (typeof ur === 'undefined') ? img.src : ur;
+                        
+                       $.imageExist(imgurl).then(function(successurl)
+                       {
+                        zImage.src=successurl;
+                        }).catch(function(errorurl)
+                        {
+                            zImage.src=img.src;
+                            console.log('Image url "'+errorurl+'" was not found, default image source has been loaded instead');
+                        });
+                        zLayer.style.display ='block';
+                        zImage.onload = function() 
+                            {
+                                $.resize(zImage.naturalWidth, zImage.naturalHeight);
+                            };
+                    });
+                    $.addEvent();
+   
         };
         $.objectLayer= function()
         {
@@ -78,7 +85,6 @@
         };
         $.resize=function resize(eW, eH)
         {        
-//            var dimension= $.p10(w.innerWidth, w.innerHeight);
             var screenW = $.p10(w.innerWidth);
             var screenH = $.p10(w.innerHeight);
 
@@ -89,7 +95,7 @@
                 }
                 else if(eW > screenW && eH < screenH)
                 {
-                   $.resize($.p10(eW), $p10(eH));
+                   $.resize($.p10(eW), $.p10(eH));
                     console.log('b '+eW+'x'+eH);
                 }
                 else if(eW < screenW && eH > screenH)
@@ -112,21 +118,12 @@
             var cW=0;
             var cH=0;
             $.removeEvent();
-//            d.querySelector('body').style.margin='0';
-//            d.querySelector('body').style.height='100%';
-//            d.querySelector('body').style.width='100%';
-//            
-//            d.querySelector('html').style.margin='0';
-//            d.querySelector('html').style.height='100%';
-//            d.querySelector('html').style.width='100%';
-//            d.getElementById('zoomImage').style.width=eW-20+'px';
-//            d.getElementById('zoomImage').style.height=eH-20+'px';
             function enlarging() 
                 {
-                  var incW = 3*(eW/eH);   
-                  var incH = 3*(eH/eW); 
-                  cW=cW+incW;  // update parameters
-                  cH=cW+incH;
+                  var incW =Math.round(3 *(eW/eH) * 100/100);   
+                  var incH =Math.round(3* (eH/eW) * 100/100); 
+                  cW=Math.round(cW+incW* 100/100);  // update parameters
+                  cH=Math.round(cW+incH* 100/100); 
                   el.style.width = cW + 'px';
                   el.style.height = cH + 'px';
                   if (cW  >= eW)  // check finish condition
@@ -134,7 +131,7 @@
                     clearInterval(timing); 
                     $.addEvent();
                     d.getElementById('zoomClose').style.top=(d.getElementById('zoomImage').offsetTop + 25)+'px';
-                    d.getElementById('zoomClose').style.right="5px";
+                    d.getElementById('zoomClose').style.right="25px";
                     d.getElementById('zoomClose').style.opacity ='1';
                   }
                 }
@@ -143,30 +140,32 @@
         
         $.reduce = function reduce(el)
         {
-                var curW = parseInt(el.style.width);
-                var curH = parseInt(el.style.height);
+                var curW = Math.round(parseInt(el.style.width)* 100/100);
+                var curH = Math.round(parseInt(el.style.height)* 100/100);
                 d.getElementById('zoomClose').style.transition='opacity 0.0s ease-in-out';
                 d.getElementById('zoomClose').style.opacity ='0';
                 
                 $.removeEvent();
                 function reducing() 
                 {
-                  var decW = 3*(curW/curH);   
-                  var decH = 3*(curH/curW);   
-                  curW=curW-decW;  // update parameters
-                  curH=curW-decH;
+                  var decW = Math.round(3*(curW/curH)* 100/100);   
+                  var decH = Math.round(3*(curH/curW)* 100/100);   
+                  curW=Math.round(curW-decW* 100/100);  // update parameters
+                  curH=Math.round(curW-decH* 100/100);
                   el.style.width = curW + 'px';
-                  el.style.height = curH + 'px';
-                  if (curW  <= 0)  // check finish condition
+                  el.style.height = curH + 'px'; 
+                  if (curW  <= 0 && curH  <= 0)  // check finish condition
                   {
-                    clearInterval(timing);
-                    $.addEvent();
+                    el.style.width = '0px';
+                    el.style.height = '0px'; 
                     d.getElementById('zoomImage').src='#';
                     d.getElementById('zoomLayer').style.display ='none';
+                    clearInterval(timing);
+                    $.addEvent();
                   }
-                }
+                };
                 var timing = setInterval(reducing, 0.5); 
-        }
+        };
         $.removeEvent= function removeEvent()
         {
             if (d.removeEventListener) 
@@ -188,12 +187,25 @@
         $.closeZoom = function closeZoom(e)
         {
             if (e.target !== this) return;
-            
             d.getElementById('zoomClose').style.opacity ='0';
             $.reduce(d.getElementById('zoomContainer'));
         };
+        $.imageExist = function imageExist(url)
+        {
+        return new Promise(function(resolve, reject){
+        var img = new Image();
+        img.onload = function(){
+            resolve(url);
+        };
+        img.onerror = function(){
+            reject(url);
+        };
+        img.src = url;
+        });
+        };
+    
         return $;
-    }
+    };
     if(typeof ($) === 'undefined'){w.$ = _();}
     else{console.log("Library already defined.");}
 })(window, document);
